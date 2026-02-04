d24c2543714f37970436d49526983b5f1df2e923
Joel Mutiso <joelwmutiso@gmail.com>
2026-02-05 01:31:35 +0300

    Task 2: Implemented imperative routing using useRouter hook in index.tsx

--- alx-project-0x03/components/common/Button.tsx
+++ alx-project-0x03/components/common/Button.tsx
@@ -1,4 +1,4 @@
-import { ButtonProps } from "@/interfaces";
+import { ... } from "@/interface";
 
 const Button = ({ buttonLabel, buttonSize, buttonBackgroundColor, action }: ButtonProps) => {
   // Add the explicit type record here:
--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,6 +1,6 @@
 import Button from "@/components/common/Button";
 import { useRouter } from "next/router";
-import { PageRouteProps } from "@/interfaces";
+import { ... } from "@/interface";
 
 export default function Home() {
   const router = useRouter();
