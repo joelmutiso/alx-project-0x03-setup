2cecec648dc64ab2042407ec48fc99807f7ef7c0
Joel Mutiso <joelwmutiso@gmail.com>
2026-02-05 00:57:24 +0300

    Integrated Montserrat Google Font and updated landing page

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,14 +1,9 @@
 const Home = () => {
   return (
-   <div className="flex flex-col items-center justify-center min-h-[60vh] text-center px-4">
-    <h1 className="text-4xl md:text-6xl font-bold text-gray-900"> 
-      Welcome to Splash App 
-    </h1>
-    <p className="mt-4 text-lg text-gray-600">
-      Experience the beauty of Montserrat typography in our shared layout.
-    </p>
+   <div>
+    <h1> Welcome to Splash App </h1>
    </div>
-  );
-};
+  )
+}
 
 export default Home;
